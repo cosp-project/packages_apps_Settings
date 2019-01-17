d6866087a37fe3fbfb5193115eef3d69d2b95f4e
DennySPB <dennyspb@gmail.com>
2019-01-17 11:46:48 +0300

    Drop Ambient Play
    not work anymore
    
    Revert "AmbientPlay: fix crash in history"
    
    This reverts commit 19cb66143dea865f2132da79c1a30749ceb62945.
    
    Change-Id: I8617d8e7be4cf3458079bac63c1520e893f8394c
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    Revert "SettingsGateway: publish for ambient play tile"
    
    This reverts commit f75ab6d66ddf3c4312e91ccc62cfc4a43b11571a.
    
    Change-Id: I6a9309e5c74c8137560a7f9455c3bbfceb97de53
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    Revert "AmbientPlay: Introduce history manager [4/4]"
    
    This reverts commit 4ec510c40a6eeed55a61ca188aafaf8e9c361313.
    
    Change-Id: Ib35ec8d9a630a7b8fbe20e1b86d354d95f46e1ac
    Signed-off-by: DennySPB <dennyspb@gmail.com>
    
    Revert "Add preference for Ambient Play"
    
    This reverts commit 59041615296eb88f5b7fb0c981fddb18989f827d.
    
    Change-Id: If322a6711833890642f19e773e60d648a6b9c1fc
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- src/com/android/settings/Settings.java
+++ src/com/android/settings/Settings.java
@@ -166,5 +166,4 @@ public class Settings extends SettingsActivity {
     public static class AccountDashboardActivity extends SettingsActivity {}
     public static class SystemDashboardActivity extends SettingsActivity {}
     public static class COSPSettingsActivity extends SettingsActivity {}
-
 }
