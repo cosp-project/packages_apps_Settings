DirtyUnicorns/android_packages_apps_Settings

Active edge settings 6/?
Change-Id: Ie096239065fee6206e134fa812b788b53baff167

src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -99,6 +99,7 @@ import com.android.settings.wfd.WifiDisplaySettings;
 import com.android.settings.wifi.ConfigureWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 
+import com.dirtyunicorns.tweaks.fragments.ActiveEdge;
 import com.dirtyunicorns.tweaks.fragments.BatteryOptions;
 import com.dirtyunicorns.tweaks.fragments.ButtonBrightness;
 import com.dirtyunicorns.tweaks.fragments.Buttons;
@@ -216,6 +217,7 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(BluetoothDashboardFragment.class);
 
         // DU Tweaks
+        addIndex(ActiveEdge.class);
         addIndex(BatteryOptions.class);
         addIndex(ButtonBrightness.class);
         addIndex(Buttons.class);

cosp-project/packages_apps_Settings

src/com/android/settings/search/SearchIndexableResourcesImpl.java
@@ -99,6 +99,8 @@ import com.android.settings.wfd.WifiDisplaySettings;
 import com.android.settings.wifi.ConfigureWifiSettings;
 import com.android.settings.wifi.WifiSettings;
 
+import com.cosp.settings.fragments.ActiveEdge;
+
 import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
@@ -193,6 +195,7 @@ public class SearchIndexableResourcesImpl implements SearchIndexableResources {
         addIndex(RecentLocationRequestSeeAllFragment.class);
         addIndex(PreviouslyConnectedDeviceDashboardFragment.class);
         addIndex(BluetoothDashboardFragment.class);
+        addIndex(ActiveEdge.class);
     }
 
     @Override